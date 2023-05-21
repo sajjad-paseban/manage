51a4e60fcafaf93a5404af97556526e9086ad2e9
iamsajad <s.pr98@yahoo.com>
2023-05-21 19:09:57 +0330

    adding gh pages chnaged v0.1

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/manage/'>
     <Routes>
       <Route path='/' element={<App />}>
         <Route index element={<Dashboard />} />
