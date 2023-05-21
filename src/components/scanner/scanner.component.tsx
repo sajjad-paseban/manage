814284fe43015e556c1038034fb42096ca361825
iamsajad <s.pr98@yahoo.com>
2023-05-21 19:54:03 +0330

    adding gh pages chnaged v0.2

--- src/components/scanner/scanner.component.tsx
+++ src/components/scanner/scanner.component.tsx
@@ -1,26 +1,31 @@
 import { Html5Qrcode } from "html5-qrcode";
 import React from "react";
 import './scanner.style.scss';
+import { FaStop } from "react-icons/fa";
 
 export class Scanner extends React.Component<any,any>{
 
+
+    
     constructor(props: any){
         super(props);
     }
 
+
+    private html5QrCode!: Html5Qrcode;
     async componentDidMount() {
         const deviceId = await Html5Qrcode.getCameras().then(value => value[1].id)
-        const html5QrCode = new Html5Qrcode("scanner");
-        html5QrCode.start(deviceId,{
+        this.html5QrCode = new Html5Qrcode("render");
+        this.html5QrCode.start(deviceId,{
             fps: 5,
             qrbox: {
-                height: 500,
-                width: 500
+                height: 300,
+                width: 300
             }
         },(decodedText, decodedResult)=>{
             this.props.callbackData(decodedText)
             this.props.scannerStateHandler(0)
-            html5QrCode.stop()
+            this.html5QrCode.stop()
         },(error)=>{
 
         })
@@ -28,8 +33,11 @@ export class Scanner extends React.Component<any,any>{
 
     render(): React.ReactNode {
         return(
-            <div className="scanner" id="scanner">
-
+            <div className="scanner">
+                <button className="btn btn-danger btn-sm p-1 m-2" onClick={()=> {this.html5QrCode.stop(); this.props.scannerStateHandler(0);}}>
+                    <FaStop size={25} />
+                </button>
+                <div className="render" id="render"></div>
             </div>
         );
     }
